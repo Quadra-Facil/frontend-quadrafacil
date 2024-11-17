79c5f13b0d8e00959fde21210ee2bb9de2b63a7c
Alan César <alancarloscesar@gmail.com>
2024-11-17 08:58:57 -0300

    fix: login

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import './App.css'
+import LogoMarca from './img/logomarca-branca.svg'
+
+export default function App() {
+
+  return (
+    <>
+      <section className='login-container'>
+        <article className="login">
+          <img src={LogoMarca} alt="logo" className='logo' />
+          <h1>Sign In</h1>
+          <form>
+            <h3>Entre em sua conta</h3>
+            <input type="email"
+              placeholder='Email'
+              required
+            />
+            <input type="password"
+              placeholder='Senha'
+              required
+            />
+            <div className='area-link-forgot-pass'>
+              <a href="#">Esqueci minha senha</a>
+            </div>
+            <button>Entrar</button>
+          </form>
+        </article>
+
+        <article className="information">
+          <h1>Crie sua conta</h1>
+          <h3>Crie sua conta e comece a usar nossas funcionalidades.</h3>
+          <a href="">Criar Conta</a>
+        </article>
+
+      </section>
+    </>
+  )
+}
