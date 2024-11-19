555c4cd92a45ede1b46b482a9049c45d179b521d
Alan César <alancarloscesar@gmail.com>
2024-11-19 19:36:45 -0300

    fix: add loading animation

--- src/Pages/SignOut/index.tsx
+++ src/Pages/SignOut/index.tsx
@@ -1,11 +1,45 @@
-import { Link } from "react-router-dom"
-import "./style-signout.css"
+import { Link } from "react-router-dom";
+import "./style-signout.css";
+import { FormEvent, useState } from "react";
+import { api } from "../../services/apiClient";
+import Loading from "../../components/Loading";
 
 export default function SignOut() {
+  const [userName, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordRepeat, setPasswordRepeat] = useState('');
+  const [isLoading, setIsLoading] = useState(false); // Inicializando como false (não está carregando no início)
+
+  async function handleAddUser(e: FormEvent) {
+    e.preventDefault();
+
+    if (password !== passwordRepeat) {
+      alert("As senhas não coincidem! Por favor, repita a senha corretamente.");
+      return;
+    }
+
+    setIsLoading(true);
+
+    try {
+      const response = await api.post('/api/user', {
+        userName,
+        email,
+        password,
+        phone
+      });
+      console.log(response.data);
+      setIsLoading(false);
+    } catch (error: any) {
+      console.log(error.response?.data?.erro || 'Erro desconhecido');
+      setIsLoading(false);
+    }
+  }
+
   return (
     <>
       <section className='signout-container'>
-
         <article className="information">
           <h1>Faça seu login</h1>
           <h3>Se você já tem uma conta, faça seu login.</h3>
@@ -14,33 +48,47 @@ export default function SignOut() {
 
         <article className="signout">
           <h1>Sign Out</h1>
-          <form>
-            <input type="text"
-              placeholder='Nome'
-              required
-            />
-            <input type="email"
-              placeholder='Email'
-              required
-            />
-            <input type="text"
-              placeholder='Telefone'
-              required
-            />
-            <input type="password"
-              placeholder='Senha'
-              required
-            />
-            <input type="password"
-              placeholder='Repetir Senha'
-              required
-            />
 
-            <button>Cadastrar</button>
-          </form>
+          {/* Condicional de loading */}
+          {isLoading ? (
+            <Loading />
+          ) : (
+            <form onSubmit={handleAddUser}>
+              <input
+                type="text"
+                placeholder='Nome'
+                required
+                onChange={e => setName(e.target.value)}
+              />
+              <input
+                type="email"
+                placeholder='Email'
+                required
+                onChange={e => setEmail(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder='Telefone'
+                required
+                onChange={e => setPhone(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder='Senha'
+                required
+                onChange={e => setPassword(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder='Repetir Senha'
+                required
+                onChange={e => setPasswordRepeat(e.target.value)}
+              />
+              <button type="submit">Cadastrar</button>
+            </form>
+          )}
         </article>
-
       </section>
     </>
-  )
+  );
 }
--- src/Pages/main.tsx
+++ src/Pages/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SignOut from './SignOut/index.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
