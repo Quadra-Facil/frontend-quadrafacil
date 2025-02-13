58072d92e1b87712a115fc45cd976f2428a3a5fc
Alan César <alancarloscesar@gmail.com>
2025-02-13 09:05:54 -0300

    feat: validation timers with expedient finished

--- src/components/DatePickerReserve/index.tsx
+++ src/components/DatePickerReserve/index.tsx
@@ -3,7 +3,7 @@ import "./style.css"; // Importando o arquivo CSS customizado
 import { useContext, useEffect, useState } from "react";
 import { setHours, setMinutes, setSeconds, differenceInMilliseconds, format, getHours, isBefore } from "date-fns";
 import { DayPicker, Locale } from "react-day-picker";
-import { ptBR } from "date-fns/locale";
+import { pt, ptBR } from "date-fns/locale";
 import Toast from "../Toast";
 import Loading from "../Loading";
 import LoadingReserve from "../../img/loadingreserve.json";
@@ -17,6 +17,8 @@ import { AuthContext } from "../../services/contexts/AuthContext";
 import DatePickerHourReserved from "../DatePickerHourReserved";
 import ModalSerchArena from "../ModalSearchArena";
 import Lottie from "lottie-react";
+import Logo from "../../img/logomarca.svg"
+
 
 interface Space {
   spaceId: number;
@@ -60,6 +62,27 @@ interface PromotionResponse {
   $values: Promotion[];
 }
 
+interface WeekDays {
+  $id: string;
+  $values: number[];
+}
+
+interface ArenaSchedule {
+  $id: string;
+  id: number;
+  arenaId: number;
+  weekDays: WeekDays;
+  startTime: string;
+  endTime: string;
+  open: boolean;
+}
+
+interface ScheduleResponse {
+  $id: string;
+  $values: ArenaSchedule[];
+}
+
+
 
 export function DatePickerReserve() {
   const [selected, setSelected] = useState<Date | undefined>(undefined);
@@ -84,6 +107,14 @@ export function DatePickerReserve() {
   const [getPromoType, setGetPromoType] = useState<string>('')
 
   const [valueGetPromo, setValueGetPromo] = useState<any>()
+
+  const [loadExpedient, setLoadExpedient] = useState<ScheduleResponse | undefined>();
+  const [startTimeExp, setStartTimeExp] = useState<string>('');
+  const [endTimeExp, setEndTimeExp] = useState<string>('');
+  const [formattedDate, setFormattedDate] = useState<string>('');
+  const [expedientMessage, setExpedientMessage] = useState<string>('');
+  const [isOpenInforme, setIsOpenInforme] = useState<boolean>(false);
+  const [messageInformeError, setMessageInformeError] = useState<string>('')
 
   const navigate = useNavigate();
   const authContext = useContext(AuthContext);
@@ -156,18 +187,203 @@ export function DatePickerReserve() {
 
   const roundMinutes = (minutes: number): number => minutes <= 15 ? 0 : 30;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // funciona
   const handleTimeChange = (event: React.ChangeEvent<HTMLInputElement>, isStart: boolean) => {
     const time = event.target.value;
     const [hour, minute] = time.split(":").map(Number);
     const roundedMinute = roundMinutes(minute);
     const formattedTime = `${String(hour).padStart(2, "0")}:${String(roundedMinute).padStart(2, "0")}:00`;
 
+    // Se é o Start Time
     if (isStart) {
       setStartTime(formattedTime);
-    } else {
+      // console.log("Horário de início selecionado:", formattedTime);
+      checkTimeValidity(formattedTime, "start");
+    }
+    // Se é o End Time
+    else {
       setEndTime(formattedTime);
+      // console.log("Horário de fim selecionado:", formattedTime);
+      checkTimeValidity(formattedTime, "end");
     }
   };
+
+  const checkTimeValidity = (selectedTime: string, type: "start" | "end") => {
+    if (!loadExpedient) {
+      console.log("Expediente não carregado.");
+      return;
+    }
+
+    // Se não houver data selecionada, define como a data de hoje
+    const selectedDate = selected ? new Date(selected) : new Date(); // Usa a data atual se 'selected' não estiver definido
+
+    // Obter o dia da semana baseado na data selecionada
+    const selectedDay = selectedDate.getDay(); // Usando a data selecionada ou a data de hoje
+    const adjustedSelectedDay = selectedDay === 0 ? 7 : selectedDay; // Se o dia selecionado for domingo, ajusta para 7
+
+    // Encontrar o expediente para o dia selecionado
+    const expedientForSelectedDay = loadExpedient.$values.find((expedient) =>
+      expedient.weekDays.$values.includes(adjustedSelectedDay)
+    );
+
+    if (expedientForSelectedDay) {
+      const { startTime: expStartTime, endTime: expEndTime } = expedientForSelectedDay;
+
+      // Converte os horários de expediente e os horários selecionados em objetos Date para facilitar a comparação
+      const [expStartHour, expStartMinute] = expStartTime.split(":").map(Number);
+      const [expEndHour, expEndMinute] = expEndTime.split(":").map(Number);
+      const [selectedStartHour, selectedStartMinute] = selectedTime.split(":").map(Number);
+
+      // Verificar se a hora e minuto são válidos
+      if (isNaN(expStartHour) || isNaN(expStartMinute) || isNaN(expEndHour) || isNaN(expEndMinute) || isNaN(selectedStartHour) || isNaN(selectedStartMinute)) {
+        setMessageInformeError("Horário inválido.");
+        openModalInformeExp();
+        return;
+      }
+
+      const expStart = new Date();
+      expStart.setHours(expStartHour, expStartMinute, 0);
+
+      const expEnd = new Date();
+      expEnd.setHours(expEndHour, expEndMinute, 0);
+
+      const selectedTimeObj = new Date();
+      selectedTimeObj.setHours(selectedStartHour, selectedStartMinute, 0);
+
+      // Validação para startTime
+      if (type === "start") {
+        if (selectedTimeObj >= expStart) {
+          console.log("Horário de início válido.");
+        } else {
+          setMessageInformeError(`Arena ainda não estará aberta no horário inicial, selecione a partir das ${format(expStart, "HH'h'")}.`);
+          openModalInformeExp();
+          setStartTime('00:00');
+          return;
+        }
+
+        // Validação para verificação de data e hora atual
+        const todayDate = format(new Date(), "yyyy-MM-dd");
+        const selectedDateFormatted = format(selectedDate, "yyyy-MM-dd"); // Formata para "yyyy-MM-dd"
+
+        if (todayDate === selectedDateFormatted) {
+          const currentTime = new Date(); // Hora atual
+
+          // Comparar hora atual com a hora selecionada
+          if (currentTime > selectedTimeObj) {
+            setMessageInformeError(`Este horário inicial já passou.`);
+            openModalInformeExp();
+            setStartTime('00:00');
+            return;
+          }
+        }
+      }
+
+      // Validação para endTime
+      if (type === "end") {
+        const [selectedEndHour, selectedEndMinute] = selectedTime.split(":").map(Number);
+        const selectedEndTimeObj = new Date();
+        selectedEndTimeObj.setHours(selectedEndHour, selectedEndMinute, 0);
+
+        // Verificar se a hora de término é válida
+        if (isNaN(selectedEndHour) || isNaN(selectedEndMinute)) {
+          setMessageInformeError("Horário final inválido.");
+          openModalInformeExp();
+          return;
+        }
+
+        // Validação do horário de fim em relação ao expediente
+        if (selectedEndTimeObj <= expEnd) {
+          console.log("Horário de fim válido.");
+        } else {
+          setMessageInformeError(`Horário final fora do expediente da arena, selecione a partir até ${format(expEnd, "HH'h'")}.`);
+          openModalInformeExp();
+          setEndTime('00:00');
+          return;
+        }
+
+        // Validação adicional para garantir que endTime seja maior ou igual a startTime
+        if (selectedEndTimeObj < expStart) {
+          setMessageInformeError(`Selecione um horário das ${format(expStart, "HH'h'")} até ${format(expEnd, "HH'h'")} para o dia selecionado.`);
+          openModalInformeExp();
+          setStartTime('00:00');
+          setEndTime('00:00');
+          return;
+        }
+      }
+    } else {
+      setMessageInformeError("Expediente para o dia selecionado não encontrado.");
+      openModalInformeExp();
+      setStartTime('00:00');
+      setEndTime('00:00');
+      return;
+    }
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   useEffect(() => {
     if (user?.arena) {
@@ -182,7 +398,7 @@ export function DatePickerReserve() {
           setSendMessage(error?.response?.data);
         });
     }
-  }, [user?.arena]); // Só executa quando o arenaId mudar
+  }, [arenaId]); // Só executa quando o arenaId mudar
 
   useEffect(() => {
     if (startTime && endTime && loadPromotions.length > 0) {
@@ -361,6 +577,135 @@ export function DatePickerReserve() {
     setSelectedSpaceReserved(id);
   };
 
+  //expediente
+
+  // Função para obter dados de expediente
+  async function GetExpedient() {
+    try {
+      const response = await api.post("/api/ArenaHours/get", {
+        arenaId: arenaId
+      });
+
+      // Definindo o estado com os valores recebidos
+      const expedientData = response?.data;
+      setLoadExpedient(expedientData); // O objeto retornado já é do tipo ScheduleResponse
+    } catch (error: any) {
+      setSendTitle('error');
+      setSendMessage(`${error?.response?.data}`);
+    }
+  }
+
+  // Função para pegar o "weekDay" da data selecionada ou da data de hoje
+  function getWeekDay(date: Date) {
+    const dayOfWeek = date.getDay();
+    return dayOfWeek === 0 ? 7 : dayOfWeek; // Ajusta para domingo ser 7, e os outros dias de segunda a sábado como 1 a 6
+  }
+
+  // Chama GetExpedient sempre que o modal for aberto
+  useEffect(() => {
+    if (modalIsOpenReserve) {
+      GetExpedient();
+    }
+  }, [modalIsOpenReserve]);
+
+  useEffect(() => {
+    if (loadExpedient && loadExpedient.$values.length > 0) {
+      // Verifica a data selecionada ou usa a data atual
+      const selectedDate = selected ? selected : new Date(); // Se houver data selecionada, usa ela. Caso contrário, usa a data atual.
+      const adjustedGetDay = getWeekDay(selectedDate); // Obtém o dia ajustado da semana para a data selecionada
+
+      // Filtra o expediente com base no weekDay
+      const getExpToday = loadExpedient.$values.filter((item: ArenaSchedule) => {
+        return item.weekDays?.$values.includes(adjustedGetDay);
+      });
+
+      if (getExpToday.length > 0) {
+        // Verifica se o expediente está aberto (open === true)
+        const currentExpedient = getExpToday[0];
+
+        if (!currentExpedient.open) {
+          openModalInformeExp()
+          setExpedientMessage('Arena fechada');
+          setStartTimeExp('');
+          setEndTimeExp('');
+        } else {
+          // Formatando o horário para mostrar apenas horas e minutos
+          const startTimeFormatted = currentExpedient.startTime.substring(0, 5); // Pega "HH:mm"
+          const endTimeFormatted = currentExpedient.endTime.substring(0, 5); // Pega "HH:mm"
+
+          setStartTimeExp(startTimeFormatted); // Atualiza o estado com o horário de início
+          setEndTimeExp(endTimeFormatted); // Atualiza o estado com o horário de término
+          setExpedientMessage(''); // Limpa a mensagem de "Sem expediente"
+        }
+      } else {
+        // Se não encontrar expediente para o dia, limpa os estados e exibe a mensagem
+        setStartTimeExp('');
+        setEndTimeExp('');
+        setExpedientMessage('⏰');
+      }
+    }
+  }, [loadExpedient, selected]); // Executa quando loadExpedient ou selected (data selecionada) mudarem
+
+  // Função para formatar a data
+  function formatSelectedDate(selected: Date | undefined) {
+    const today = new Date();
+
+    if (!selected) {
+      return format(today, "eeee dd/MM", { locale: pt }); // Se não houver data selecionada, retorna a data de hoje
+    }
+
+    const isToday = selected.toDateString() === today.toDateString();
+
+    if (isToday) {
+      return "Hoje"; // Se for hoje, retorna "Hoje"
+    }
+
+    // Se for outro dia, retorna o nome do dia e a data no formato "Dia da Semana dd/MM"
+    return format(selected, "eeee dd/MM", { locale: pt });
+  }
+
+  useEffect(() => {
+    // Verifica se há data selecionada e formata a data
+    const formattedDate = formatSelectedDate(selected);
+
+    // Aqui você pode configurar a mensagem ou a data formatada
+    setFormattedDate(formattedDate);
+
+  }, [selected]);
+
+  const customStylesModalInforme = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      backgroundColor: '#fff',
+      border: '0px solid #ccc',
+      borderRadius: '10px',
+      padding: '0px',
+      boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+      width: '40vw',
+      height: '50vh',
+      maxWidth: '100%',
+      color: '#6c6c6c'
+    },
+    overlay: {
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+  };
+
+
+  function openModalInformeExp() {
+    setIsOpenInforme(true);
+  }
+
+  function closeModalInformeExp() {
+    setIsOpenInforme(false);
+    // closeModalReserve();
+  }
+
   return (
     <>
       <Toast title={sendTitle} message={sendMessage} />
@@ -369,6 +714,16 @@ export function DatePickerReserve() {
           <Modal isOpen={modalIsOpenReserve} onRequestClose={closeModalReserve} style={customStylesModalReserve} shouldCloseOnOverlayClick={false}>
             <header className="header-modal">
               <h1><strong>{arena}</strong></h1>
+              <p>{formattedDate}</p>
+
+              {/* Exibe a mensagem de expediente ou a mensagem de sem expediente */}
+              {expedientMessage ? (
+                <p>{expedientMessage}</p>
+              ) : (
+                <>
+                  <p>Aberto: {startTimeExp} às {endTimeExp}</p>
+                </>
+              )}
               <div className="area-close" onClick={closeModalReserve}><FiX size={24} /></div>
             </header>
             <div className="main-reserve">
@@ -488,11 +843,49 @@ export function DatePickerReserve() {
 
                 <h5>Promoção: {promoTypeResult || 'Nenhuma promoção encontrada'}</h5>
 
-
                 <button className="btn-reserve" onClick={calculeHoursAndCreateReserve}>Reservar</button>
               </section>
             </div>
           </Modal>
+
+
+          {/* modal informe */}
+          <Modal
+            isOpen={isOpenInforme}
+            onRequestClose={closeModalInformeExp}
+            style={customStylesModalInforme}
+            shouldCloseOnOverlayClick={false}
+          >
+            <header className="header-modal-informe">
+
+              <img src={Logo} alt="logo" />
+
+              <div className="area-close" onClick={closeModalInformeExp}>
+                <FiX size={24} />
+              </div>
+            </header>
+            <section className="main-modal-informe">
+              <h1>Fora do expediente =(</h1>
+
+              {
+                expedientMessage !== '' ?
+                  <h5><strong>Informe:</strong> {expedientMessage} para <strong>{formattedDate}</strong></h5>
+                  :
+                  <h5><strong>Informe:</strong> {messageInformeError}</h5>
+              }
+
+              {
+                user?.role === "admin" && (
+                  <h5>Expediente em: configs/expediente</h5>
+                )
+              }
+
+              <button onClick={() => closeModalInformeExp()}>Fechar</button>
+
+            </section>
+
+          </Modal>
+
         </>
       )}
     </>
